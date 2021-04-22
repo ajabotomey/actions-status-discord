d3f8506d1274220247cac13516c9853f802b2d55
AJ Abotomey <killerbyte_93@hotmail.com>
2021-04-22 17:17:08 +1000

    RunID is now RunId

--- src/main.ts
+++ src/main.ts
@@ -41,13 +41,13 @@ function wrapWebhook(webhook: string, payload: Object): Promise<void> {
 export function getPayload(inputs: Readonly<Inputs>): Object {
     const ctx = github.context
     const { owner, repo } = ctx.repo
-    const { eventName, sha, ref, workflow, actor, payload, runID } = ctx
+    const { eventName, sha, ref, workflow, actor, payload, runId} = ctx
     const repoURL = `https://github.com/${owner}/${repo}`
     // if the trigger is pull_request, check `github.event.pull_request.head.sha` first.
     // see issues/132
     const validSHA = ctx.payload.pull_request?.head?.sha || sha
-    // const workflowURL = `${repoURL}/commit/${validSHA}/checks`
-    const workflowURL = `${repoURL}/actions/runs/${runID}`
+    //const workflowURL = `${repoURL}/commit/${validSHA}/checks`
+    const workflowURL = `${repoURL}/actions/runs/${runId}`
     
 
     logDebug(JSON.stringify(payload))
